1f95448fe00fe7553ed6e5e88e05cb133a6320ab
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2023-06-18 07:27:54 -0300

    ultima atualização para mobile ok

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function nextImage() {
 }
 
 function clickMenu(){
-    if(itens.style.display == 'block'){
+    if(itens.style.display=='block'){
         itens.style.display='none'
     }else{
         itens.style.display='block'
