cdaeedc2e80a9542c8b4adcfbde00131a4752079
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2023-06-18 06:43:33 -0300

    nd

--- script.js
+++ script.js
@@ -18,6 +18,6 @@ function clickMenu(){
     if(itens.style.display == 'block'){
         itens.style.display='none'
     }else{
-        itens.style.display = 'block'
+        itens.style.display='block'
     }
 }
